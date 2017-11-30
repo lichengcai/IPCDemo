cb4e45d66f66e0db7487ac6bc58438159f0d04b5
lichengcai <lichengcai@koolearn.com>
2017-11-30 16:30:58 +0800

    前台服务

---
+++ app/src/main/java/cc/ipcdemo/service/MyService.java
@@ -0,0 +1,70 @@
+package cc.ipcdemo.service;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import cc.ipcdemo.MainActivity;
+import cc.ipcdemo.R;
+
+/**
+ * Created by lichengcai on 2017/11/30.
+ */
+
+public class MyService extends Service {
+    public static final String TAG = "MyService";
+    private MyBinder mBinder = new MyBinder();
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //设置为前台服务
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+        Notification notification = new Notification.Builder(getApplicationContext())
+                .setAutoCancel(true)
+                .setContentTitle("title")
+                .setContentText("describe")
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setWhen(System.currentTimeMillis())
+                .build();
+        startForeground(1, notification);
+        Log.d(TAG, "onCreate() executed");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand() executed");
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    public class MyBinder extends Binder {
+        public void startDownload() {
+            Log.d("TAG", "startDownload() executed");
+            // 执行具体的下载任务
+        }
+    }
+}
